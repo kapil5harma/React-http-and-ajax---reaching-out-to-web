d4c74e2f2705164bf3f31ae6fb4b41b56c557462
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-06-29 23:42:42 +0530

    Using Axios Interceptors (Request interceptor and response interceptor)

--- src/index.js
+++ src/index.js
@@ -3,6 +3,33 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
 
-ReactDOM.render( <App />, document.getElementById( 'root' ) );
+axios.interceptors.request.use(
+  request => {
+    console.log('request: ', request);
+    // Edit request config here, (like adding global headers, etc)
+    return request;
+  },
+  error => {
+    console.log('error: ', error);
+    // Do something if your config fails (like there's no internet connection)
+    return Promise.reject(error);
+  }
+);
+
+axios.interceptors.response.use(
+  response => {
+    console.log('response: ', response);
+    // Edit response config here, (like adding global headers, etc)
+    return response;
+  },
+  error => {
+    console.log('error: ', error);
+    // Do something if your config fails (like there's no internet connection)
+    return Promise.reject(error);
+  }
+);
+
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();

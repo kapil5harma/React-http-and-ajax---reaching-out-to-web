96a8c54e9f1d3ec1b6ccd09002a24fdd4bbccce2
Kapil Sharma <kapilsharmajmps@gmail.com>
2018-07-03 20:57:54 +0530

    Routing and Server Deployment

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Blog from './containers/Blog/Blog';
 class App extends Component {
   render() {
     return (
+      // <BrowserRouter basename="/">  // Default basename is root
+      // Set basename="my-app" if your app is not at www.example.com but at www.example.com/my-app
       <BrowserRouter>
         <div className="App">
           <Blog />
